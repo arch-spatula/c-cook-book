53fdf3a311d8cccfeda1b6f83b0d3b7893bbda7e
김상현 <84452145+arch-spatula@users.noreply.github.com>
2024-04-05 22:45:35 +0900

    줍줍

--- test/test.c
+++ test/test.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main(void) {
-  char ch1, ch2;
-  printf("Input the first character:"); // Line 1
-  scanf("%c", &ch1);
-  printf("Input the second character:"); // Line 2
-  ch2 = getchar();
-
-  printf("ch1=%c, ASCII code = %d\n", ch1, ch1);
-  printf("ch2=%c, ASCII code = %d\n", ch2, ch2);
-
-  system("PAUSE");
+	
+  char szName[32][4] = {0};
   return 0;
-
 }
